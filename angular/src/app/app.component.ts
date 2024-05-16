ceeaab2d8e706f016248aebbee2fee680846cde2
David <david.santiagonino20@gmail.com>
2024-05-15 22:01:46 -0500

    angular

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import{InicioComponent} from"./components/inicio/inicio.component"
+import{ServiciosComponent } from"./components/servicios/servicios.component"
+import{ProductosComponent} from"./components/productos/productos.component"
+import{NosotrosComponent}from"./components/nosotros/nosotros.component"
+import{ContactoComponent}from"./components/contacto/contacto.component"
+import{RegistrarseComponent}from"./components/registrarse/registrarse.component"
+import{IniciarSesionComponent}from"./components/iniciar-sesion/iniciar-sesion.component"
+import{PrivadoComponent}from"./components/privado/privado.component"
+import{NoEncontradoComponent}from"./components/no-encontrado/no-encontrado.component"
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular';
+}
---
+++ angular/src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import {InicioComponent} from "./components/inicio/inicio.component"
+import { Component } from '@angular/core';
+import{ProductosComponent} from"./components/productos/productos.component"
+import{ServiciosComponent } from"./components/servicios/servicios.component"
+import{NosotrosComponent}from"./components/nosotros/nosotros.component"
+import{ContactoComponent}from"./components/contacto/contacto.component"
+import{RegistrarseComponent}from"./components/registrarse/registrarse.component"
+import{IniciarSesionComponent}from"./components/iniciar-sesion/iniciar-sesion.component"
+import{PrivadoComponent}from"./components/privado/privado.component"
+import{NoEncontradoComponent}from"./components/no-encontrado/no-encontrado.component"
+
+
+export const routes: Routes = [
+    {path: "inicio",component: InicioComponent, title: "Iniciooo"},
+    {path: "productos",component:ProductosComponent},
+    {path: "servicios",component:ServiciosComponent},
+    {path:"nosotros",component:NosotrosComponent},
+    {path:"contacto",component:ContactoComponent},
+    {path:"registrarse",component:RegistrarseComponent},
+    {path:"iniciar sesion",component:IniciarSesionComponent},
+    {path:"privado",component:PrivadoComponent},
+    {path:"",redirectTo:"inicio",pathMatch:"full"},
+    {path:"**",component: NoEncontradoComponent, title: "Error, la pagina solicitada no existe"},
+    
+];
